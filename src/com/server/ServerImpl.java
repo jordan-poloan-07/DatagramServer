47e3d40e9e0a5ebc45ba6413034d1d0e2b13dbf3
LostSeraph <jordan.poloan@gmail.com>
2013-12-03 08:32:27 +0800

    Update ServerImpl.java
    added comments in receivePackets() and sendToAll() methods

--- src/com/server/ServerImpl.java
+++ src/com/server/ServerImpl.java
@@ -27,16 +27,23 @@ public class ServerImpl {
 
 		try {
 
-			DatagramSocket socket = new DatagramSocket(4445);
+			DatagramSocket socket = new DatagramSocket(4445); // create a datagram socket in port 4445
 
-			byte[] buf = message.trim().getBytes();
+			byte[] buf = message.trim().getBytes(); // get the byte array of the message
 
-			InetAddress group = InetAddress.getByName("230.0.0.1");
+			InetAddress group = InetAddress.getByName("230.0.0.1"); // 230.0.0.1 is a valid a multicasting address
+			// server will broadcast the message in the multicast address
+			
+			// create the datagrampacket to be broadcast
+			// pass the byte array and its length and the InetAddress of the multicast address, and the port to be used
 			DatagramPacket packet = new DatagramPacket(buf, buf.length, group,
 					4446);
 
+			// send the pocket using the socket in port 4445 of the server
 			socket.send(packet);
+			// close after sending
 			socket.close();
+			
 		} catch (SocketException e) {
 			e.printStackTrace();
 		} catch (UnknownHostException e) {
@@ -53,13 +60,20 @@ public class ServerImpl {
 
 		try {
 
+			// listen for messages in port 9770
 			datagramSocket = new DatagramSocket(9770);
 
+			// create and empty byte buffer to store the received message
 			byte[] buffer = new byte[60000];
 
+			// the packet will be used to fille the byte array
 			DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+			
+			// after it receiveing a message, the packet will be filled with message
 			datagramSocket.receive(packet);
-
+			
+			// run a thread that broadcasts message to the multicast address
+			// (needed to broadcast one message of a client to other clients
 			new ServerReceiveThread(new String(buffer).trim()).start();
 			
 		} catch (Exception e) {
